e2221247ad6b72339e39a9de18e88de600dd51e9
Boris Lehečka <boris@daliboris.cz>
2026-04-02 11:40:57 +0200

    fix(tests): add node.js http-server for testing

--- test/run-website.cmd
+++ test/run-website.cmd
@@ -1,5 +1,6 @@
-REM Running IIS Express website with oXygen XML add-on meadata a zip file
-REM IIS Express can be downloaded from here: https://www.microsoft.com/en-us/download/details.aspx?id=48264
-REM Web sites can be configured in %userprofile%\Documents\IISExpress\config\applicationhost.config
-REM Site oXygen point to the local folder with oxygen-addons.xml and teilex0-oxygen-framework.zip files
-"C:\Program Files\IIS Express\iisexpress.exe" /site:oXygen
+REM Install NPM http-server first:
+REM npm install --global http-server
+REM brew install http-server
+REM See https://www.npmjs.com/package/http-server
+
+http-server ../web -o
